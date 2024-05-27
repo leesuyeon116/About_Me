4c9761f93a0a28379139dc121aa8f0233d3db64f
leesuyeon116 <suyeon873088@naver.com>
2024-05-27 21:21:14 +0900

    완성

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-let mainText = document.querySelector("h1")
+let mainText = document.querySelector("h1");
 
+function horizontalIndicator(e) {
+    horizontalUnderLine.style.left = e.currentTarget.offsetLeft + "px";
+    horizontalUnderLine.style.width = e.currentTarget.offsetWidth + "px";
+    horizontalUnderLine.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
+}
+
+// 스크롤 이벤트 리스너
 window.addEventListener('scroll', function() {
     let value = this.window.scrollY;
-    console.log("scrollY", value);
-
+    // 스크록 값이 100보다 크면 텍스트 사라짐
     if(value > 100) {
         mainText.style.animation = "disappear 1s ease-out forwards"; 
     }
